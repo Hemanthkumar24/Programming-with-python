c3dd6558cea5cdd0f6877b0a6e87483768335841
Hemanthkumar24 <42990275+Hemanthkumar24@users.noreply.github.com>
2018-09-14 10:11:28 +0530

    Add files via upload

---
+++ compare.py
@@ -0,0 +1,17 @@
+#write a program to compare two numbers
+a=int(input("enter value of a"))
+b=int(input("enter value of b"))
+if(a==b):
+	print("A is equal to B")
+if(b>a):
+    print("B is greater than B")
+if(a>b):
+	print("A is greater than B")
+	
+
+
+
+
+
+
+
